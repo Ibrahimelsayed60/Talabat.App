7334bb48fcab113cbf7a04e8a45e0c6629c3935e
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-10-13 13:08:45 -0700

    Part 09 Migration

--- Talabat.Repository/Data/Configurations/ProductConfigurations.cs
+++ Talabat.Repository/Data/Configurations/ProductConfigurations.cs
@@ -26,6 +26,8 @@ namespace Talabat.Repository.Data.Configurations
             builder.Property(p => p.Description).IsRequired();
 
             builder.Property(p => p.PictureUrl).IsRequired();
+
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
         }
     }
 }
