22441e195ade22accecee7263feb7b52bdaedfd0
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-10-13 12:54:47 -0700

    Part 08 Product Configurations

---
+++ Talabat.Repository/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Repository.Data.Configurations
+{
+    internal class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(P => P.ProductBrand)
+                    .WithMany()
+                    .HasForeignKey(P => P.ProductBrandId);
+
+            builder.HasOne(P => P.ProductType)
+                    .WithMany()
+                    .HasForeignKey(P => P.ProductTypeId);
+
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+
+            builder.Property(p => p.Description).IsRequired();
+
+            builder.Property(p => p.PictureUrl).IsRequired();
+        }
+    }
+}
--- Talabat.Repository/Data/StoreContext.cs
+++ Talabat.Repository/Data/StoreContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Entities;
@@ -13,6 +14,12 @@ namespace Talabat.Repository.Data
         public StoreContext(DbContextOptions<StoreContext> options):base(options)
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<Product> Products { get; set; }
