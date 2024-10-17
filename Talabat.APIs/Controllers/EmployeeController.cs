34f5c7604da46811b6319abf14e35bd26facf3dc
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-10-17 12:17:24 -0700

    Part 04 Employee Example Get Employees  - Draft

---
+++ Talabat.APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories;
+using Talabat.Core.Specifications;
+
+namespace Talabat.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+		private readonly IGenericRepository<Employee> _employeeRepo;
+
+		public EmployeeController(IGenericRepository<Employee> EmployeeRepo)
+        {
+			_employeeRepo = EmployeeRepo;
+		}
+
+        [HttpGet]
+		public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var Spec = new EmployeeWithDepartmentSpecifications();
+            var emplyees = await _employeeRepo.GetAllWithSpecAsync(Spec);
+            return Ok(emplyees);
+
+        }
+    }
+}
---
+++ Talabat.Core/Specifications/EmployeeWithDepartmentSpecifications.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Specifications
+{
+	public class EmployeeWithDepartmentSpecifications:BaseSpecifications<Employee>
+	{
+
+        public EmployeeWithDepartmentSpecifications()
+        {
+            Includes.Add(E => E.Department);
+        }
+
+    }
+}
